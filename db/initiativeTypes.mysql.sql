693b2177de1b79d71522805fe9696915488597ed
amanattrish <iam.amanattrish@gmail.com>
2023-05-05 11:06:08 +0530

    docs(initiativeTypes):sql script added

---
+++ db/initiativeTypes.mysql.sql
@@ -0,0 +1,9 @@
+use swiirl_deb_dev;
+
+INSERT INTO initiativeType (id, name, status) VALUES
+('WNQZDoDbcCXvUUWCskmtS', 'Computer Lab Equipment', 'active'),
+('UTsFL4wS6BH4K42GQf2QE', 'Science Lab Equipment', 'active'),
+('ljlEcRiVXAI4tdFb-2GdJ', 'Art Supplies', 'active'),
+('eIg34usAzhDCXy-ocuUCS', 'Classroom Furniture', 'active'),
+('Z-WX8zBbh68wkLtARSFpa', 'Field Trips', 'active'),
+('BfkIESqZJ2tSBCBh5Tdgv', 'Other (enter below)', 'active');
