589ee5e1ed2e34b727676afaf953bc557ea57492
punitbhesota <punnitbhesota@gmail.com>
2023-05-09 16:22:38 +0530

    get initiative types deployed

---
+++ initiative-service/getInitiativeTypes.js
@@ -0,0 +1,29 @@
+import { nanoid } from 'nanoid';
+
+import LOGGER from './utils/logger.js';
+import sendResponse from './utils/sendResponse.js';
+import { dbExecuteQuery } from './utils/dbConnect.js';
+
+const componentName = 'initiative-service/getInitiativeTypes';
+
+// Lambda Handler
+export const handler = async () => {
+  const reqId = nanoid();
+  try {
+    LOGGER.info(reqId, componentName, "Get Initiative types", null);
+    const query = `SELECT * FROM initiativeType`;
+    const dbResp = await dbExecuteQuery(query);
+
+    // finally return response
+    return sendResponse(reqId, 200, {
+      message: 'success',
+      data: dbResp
+    });
+
+  } catch (error) {
+    return sendResponse(reqId, 500, {
+      message: error.message || 'Internal Server Error',
+      error
+    });
+  }
+}
